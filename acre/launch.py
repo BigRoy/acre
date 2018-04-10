f5409dff31be2187cafc964cb77e339add19cd35
wijnand <wikoreman@gmail.com>
2018-04-10 15:21:00 +0200

    added launch module to make acre standalone

---
+++ acre/launch.py
@@ -0,0 +1,39 @@
+import os
+import pprint
+
+from . import get_tools, compute, merge, which, execute
+
+
+def launch(tools, executable, args):
+
+    tools_env = get_tools(tools.split(";"))
+    env = compute(tools_env)
+
+    env = merge(env, current_env=dict(os.environ))
+    print("Environment:\n%s" % pprint.pformat(env, indent=4))
+
+    # Search for the executable within the tool's environment
+    # by temporarily taking on its `PATH` settings
+    exe = which(executable, env)
+    if not exe:
+        raise ValueError("Unable to find executable: %s" % executable)
+
+    print("Launching: %s" % exe)
+    execute(exe, environment=env, args=args)
+
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--tools",
+                        help="The tool environments to include. "
+                             "These should be separated by `;`",
+                        required=True)
+    parser.add_argument("--executable",
+                        help="The executable to run. ",
+                        required=True)
+
+    kwargs, args = parser.parse_known_args()
+
+    launch(tools=kwargs.tools, executable=kwargs.executable, args=args)
